9c572ecf81e77021019fe5b14e2b192165bf104b
nvtc.98 <nvtc.98@gmail.com>
2021-05-26 09:31:21 +0700

    fix vbee get audio API

--- src/modules/express.js
+++ src/modules/express.js
@@ -169,15 +169,19 @@ const startServer = (port = defaultPort) => {
   });
 
   app.post("/get-vbee-audio", function (request, response) {
-    const id = _.get(request, "body.id", null);
-    console.log("get-vbee-audio", id, global.vbeeAudioData);
-    return _.get(global, `vbeeAudioData['${id}']`, null);
+    try {
+      const id = _.get(request, "body.id", null);
+      console.log("get-vbee-audio", id, global.vbeeAudioData);
+      const data = _.get(global, `vbeeAudioData['${id}']`, null);
+      response.status(200).send(data);
+      delete global.vbeeAudioData[id];
+    } catch {}
   });
 
   app.post("/get-vbee-audio-list", function (request, response) {
     const list = _.get(request, "body.list", []);
     console.log("get-vbee-audio-list", list, global.vbeeAudioData);
-    return list.map((x) => global.vbeeAudioData[x]);
+    response.status(200).send(list.map((x) => global.vbeeAudioData[x]));
   });
 
   const server = app.listen(process.env.PORT || port, (error) => {
